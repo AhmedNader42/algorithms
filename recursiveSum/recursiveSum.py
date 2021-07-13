6b132a468f7921cbf305817a761ee78fea477585
Ahmed Nader <ahmednader42@gmail.com>
2021-07-13 22:12:10 +0200

    Added the recursiveMax algorithm

--- recursiveSum/recursiveSum.py
+++ recursiveSum/recursiveSum.py
@@ -9,5 +9,18 @@ def recursiveCount(arr: []):
         return 0
     else: 
         return 1 + recursiveCount(arr[1:])
+
+
+def recursiveMax(arr: [int]):
+    if len(arr) == 2: 
+        return arr[0] if (arr[0] > arr[1]) else arr[1]
+    else :
+        firstValue = recursiveMax(arr[:2])
+        secondValue = recursiveMax(arr[2:])
+        return firstValue if firstValue > secondValue else secondValue
+
+        
+        
 print(recursiveSum([1,2,3, 10]))
 print(recursiveCount(['a', 'b', 'c']))
+print(recursiveMax([11,2,3, 10]))
