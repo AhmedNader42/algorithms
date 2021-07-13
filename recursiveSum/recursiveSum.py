62061ab84c0b0950ca1fa1b54dbd967d573f81a7
Ahmed Nader <ahmednader42@gmail.com>
2021-07-13 21:21:00 +0200

    Added the recursiveCount since it is the exact implementation.

--- recursiveSum/recursiveSum.py
+++ recursiveSum/recursiveSum.py
@@ -4,5 +4,10 @@ def recursiveSum(arr: [int]):
     else:
         return arr[0] + recursiveSum(arr[1:])
 
-
+def recursiveCount(arr: []):
+    if len(arr) == 0:
+        return 0
+    else: 
+        return 1 + recursiveCount(arr[1:])
 print(recursiveSum([1,2,3, 10]))
+print(recursiveCount(['a', 'b', 'c']))
