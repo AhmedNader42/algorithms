d7e730785266f00f9b2dce8e123b1b1f1a7e5d75
Ahmed Nader <ahmednader42@gmail.com>
2021-07-25 17:51:06 +0200

    Finished the Palindrome function and submitted successfully

--- Palindrome/isPalindrome.js
+++ Palindrome/isPalindrome.js
@@ -3,24 +3,38 @@
  * @return {boolean}
  * LeetCode URL https://leetcode.com/problems/palindrome-number/
  */
+
 var isPalindrome = function (x) {
     if (x < 0) {
         return false;
     }
 
     const strX = x.toString();
-    const strLen = strX.length;
-
-    if (strLen === 1) {
-        return true;
-    }
-
-    for (let forwardIndex = 0; forwardIndex < Math.floor((strLen - 1) / 2); forwardIndex++) {
-        for (let backIndex = strLen - 1; backIndex >= strLen / 2; backIndex--)
-            if (strX[forwardIndex] != strX[backIndex]) {
-                return false;
-            }
+    const reversedX = strX.split("").reverse().join("");
+    for (let i = 0; i < strX.length; i++) {
+        if (strX[i] !== reversedX[i]) {
+            return false;
+        }
     }
 
     return true;
 };
+
+console.log(isPalindrome(10) == false);
+console.log(isPalindrome(1001) == true);
+console.log(isPalindrome(0) == true);
+console.log(isPalindrome(121) == true);
+console.log(isPalindrome(-121) == false);
+
+// 1001
+//
+// 1 > 0 > 0 > 1
+// 1 > 0 > 0 > 1
+
+// 123
+// 1 > 2 > 3
+// 3 > 2 > 1
+
+// 12121
+// 1 > 2 > 1 > 2 > 1
+// 1 > 2 > 1 > 2 > 1
